793229c3930d4acd5e1463a9f2a1962f1e50426e
Wesley Carr <surfswell17@gmail.com>
2013-07-09 13:40:57 -0400

    First init. Necessary Gems installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookStore::Application.config.secret_token = 'ce513fd1473a3162cb763a04054dd7335d542ee910af81d9ec11bc46a3b84c929f3659435b7a6476513b87c6cd6f3770d168cd39390bd7244960e8aacdbad754'
