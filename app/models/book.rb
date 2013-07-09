66f14c19c64b365202b6d9535c58d13ae075df62
Wesley Carr <surfswell17@gmail.com>
2013-07-09 15:34:37 -0400

    Ran rollback and reset db. Re-writing migrations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ActiveRecord::Base
   attr_accessible :genre, :name, :price
 
-  has_many_and_belongs_to_many :authors, :stores
+  has_and_belongs_to_many :authors, :stores
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,5 @@
 class Store < ActiveRecord::Base
   attr_accessible :description, :name
 
-  has_many_and_belongs_to_many :books, :authors
+  has_and_belongs_to_many :books, :authors
 end
